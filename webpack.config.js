523f107310c505726d2d9abb214d393f60f72df7
Ethan Rubio <ethanrubio@gmail.com>
2016-04-22 12:01:56 -0700

    adds merge config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const merge = require('webpack-merge');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   // Entry accepts a path or an object of paths
   // entries. We'll be using the latter form
   // given it's convenient with more complex
@@ -18,3 +19,13 @@ module.exports = {
     filename: 'bundle.js'
   }
 };
+
+// Default configuration. We will return this 
+// if Webpack is called outside of npm.
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {});
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
