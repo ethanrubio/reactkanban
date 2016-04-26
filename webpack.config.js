d1a4d8d6f816179dd74a7086781a580ea05d3d10
Ethan Rubio <ethanrubio@gmail.com>
2016-04-26 12:22:42 -0700

    [refactor] refactors webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
-const NpmInstallPlugin = require('npm-install-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const validate = require('webpack-validator');
+const parts = require('./lib/parts');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -22,74 +22,33 @@ const common = {
     path: PATHS.build,
     filename: 'bundle.js'
   },
-  module: {
-    loaders: [
-      {
-        // Test expects a RegExp!
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        // Include accepts either a
-        // path or array of paths
-        // NOTE: might need to 
-        // remove node_modules
-        // and reinstall it
-        include: PATHS.app
-      }
-    ]
-  }
-}
-
-
-module.exports = merge(common, {
-  // Entry accepts a path or an object of entries.
-  // We'll be using the latter form given it's
-  // convenient with more complex configurations.
-  devtool: 'eval-source-map',
-  devServer: {
-    contentBase: PATHS.build,
-    // Enable history API fallback so HTML5
-    // History API based routing works.
-    // This is a good default that will come
-    // in handy in more complicated setups.
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true,
-    // Display only errors to reduce the
-    // amount of output.
-    stats: 'errors-only',
-    // Parse host and port from env so 
-    // this is easy to customize.
-    // 
-    // If you use Vagrant or Cloud 9, set
-    // host: process.env.HOST || '0.0.0.0';
-    //
-    // 0.0.0.0 is available to all network
-    // devices unlike default
-    // localhost
-    host: process.env.HOST,
-    port: process.env.PORT
-  },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new NpmInstallPlugin({
-      save: true // --save
-    }),
     new HtmlWebpackPlugin({
       title: 'Webpack demo'
     })
   ]
-});
+}
 
 var config;
 
 // Detect how npm is run and branch based on that
 switch(process.env.npm_lifecycle_event) {
   case 'build':
-    config = merge(common, {});
+    config = merge(
+      common,
+      parts.setupCSS(PATHS.app)
+    );
     break;
   default:
-    config = merge(common, {});
+    config = merge(
+      common,
+      parts.devServer({
+        // Customize host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT
+      }),
+      parts.setupCSS(PATHS.app)
+    );
 }
 
 module.exports = validate(config);
