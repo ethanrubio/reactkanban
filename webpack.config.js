2356bbdc8debe11750239d1c91cb6b69348f022c
Ethan Rubio <ethanrubio@gmail.com>
2016-04-26 20:58:41 -0700

    [feature] adds webpack extract bundle and mini chunking

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ const common = {
   },
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -40,6 +40,10 @@ switch(process.env.npm_lifecycle_event) {
         'process.env.NODE_ENV',
         'production'
       ),
+      parts.extractBundle({
+        name: 'vendor',
+        entries: ['react']
+      }),
       parts.minify(),
       parts.setupCSS(PATHS.app)
     );
