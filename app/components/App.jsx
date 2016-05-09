86f98b0043e11d4a6f6da38f8cc4c13d8b468c07
Ethan Rubio <ethanrubio@gmail.com>
2016-05-08 21:47:44 -0700

    [refactor] refactors notes to use state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import Note from './Note.jsx';
+import uuid from 'node-uuid';
 
 export default class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn Scala'
+        }
+      ]
+    };
+  }
+  
   render() {
-    const notes = [
-      {
-        id: '4a068c42-75b2-4ae2-bd0d-284b4abbb8f0',
-        task: 'Learn Webpack!'
-      },
-      {
-        id: '4e81fc6e-bfb6-419b-93e5-0242fb6f3f6a',
-        task: 'Learn React'
-      },
-      {
-        id: '11bbffc8-5891-4b45-b9ea-5c99aadf870f',
-        task: 'Learn Scala'
-      }
-    ];
+    
+    const notes = this.state.notes;
     
     return (
       <div>
