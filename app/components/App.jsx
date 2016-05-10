640cb9f32a1f632079f1d1ae5e9acb85d11f2248
Ethan Rubio <ethanrubio@gmail.com>
2016-05-10 16:10:19 -0700

    [refactor] changes app to use alt architecture

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
-import uuid from 'node-uuid';
 import Notes from './Notes.jsx';
+
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
   
   constructor(props) {
     super(props);
     
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn Webpack'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Learn Scala'
-        }
-      ]
-    };
+    this.state = NoteStore.getState();
     
     // this.addNote = this.addNote.bind(this)
+  }
+  
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged);
+  }
+  
+  componentWillUnmount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+  
+  // experimental property intializer
+  storeChanged = (state) => {
+    // Without a property initializer `this` wouldn't
+    // point at the right context because it defaults to
+    // `undefined` in strict mode.
+    this.setState(state);
   }
   
   render() {
@@ -43,42 +46,25 @@ export default class App extends React.Component {
     );
   }
   
-  // experimental property intializer
-  addNote = () => {
-    
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
-    });
-  };
-  
-  editNote = (id, task) => {
-    // don't modify if trying to set an empty value
-    if (!task.trim()) {
-      return;
-    }
-    
-    const notes = this.state.notes.map(note => {
-      if (note.id === id && task) {
-        note.task = task;
-      }
-      
-      return note;
-    });
-    
-    this.setState({notes});
-  };
-  
-  deleteNote = (id, e) => {
+  deleteNote(id, e) {
     // avoid bubbling to edit
     e.stopPropagation();
     
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
-    });
-  };
+    NoteActions.delete(id);
+  }
+  
+  addNote() {
+    NoteActions.create({task: 'New task'});
+  }
+  
+  editNote(id, task) {
+    // don't modify if trying to set an empty value
+    if(!task.trim()) {
+      return;
+    }
+    
+    NoteActions.update({id, task});
+  }
   
   // could also be written as with binding at constructor
   // addNote() {
