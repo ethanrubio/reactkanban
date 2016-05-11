3fd56dfcf14054964fe9459a9a67df1a5352083a
Ethan Rubio <ethanrubio@gmail.com>
2016-05-11 10:55:02 -0700

    [feature] adds rendering to lane

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -6,6 +6,25 @@ import NoteStore from '../stores/NoteStore';
 
 export default class Lane extends React.Component {
   render() {
+    const {lane, ...props} = this.props;
     
+    return (
+      <div {..props}>
+        <div className="lane-header">
+          <div className="lane-add-note">
+            <button onClick={this.addNote}>+</button>
+          </div>
+          <div className="lane-name">{lane.name}</div>
+        </div>
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            notes: () => NoteStore.getState().notes || []
+          }}
+        >
+          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        </AltContainer>
+      </div>
+    );
   }
 }
