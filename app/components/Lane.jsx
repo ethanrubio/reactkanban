e5699ea59954da72a31db55609eae49e3342045b
Ethan Rubio <ethanrubio@gmail.com>
2016-05-11 10:51:12 -0700

    [chore] sets up structure for lane

---
+++ app/components/Lane.jsx
@@ -0,0 +1,11 @@
+import AltContainer from 'alt-container';
+import React from 'react';
+import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
+
+export default class Lane extends React.Component {
+  render() {
+    
+  }
+}
