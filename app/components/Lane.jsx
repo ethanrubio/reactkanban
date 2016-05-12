c2ed1bdc2b9cca4eaace3f1518c83911bffc278c
Ethan Rubio <ethanrubio@gmail.com>
2016-05-11 20:40:50 -0700

    [refactor] refactors to use editable abstraction logic

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -4,6 +4,7 @@ import Notes from './Notes.jsx';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 import LaneActions from '../actions/LaneActions';
+import Editable from './Editable.jsx';
 
 export default class Lane extends React.Component {
   render() {
@@ -11,11 +12,18 @@ export default class Lane extends React.Component {
     
     return (
       <div {...props}>
-        <div className="lane-header">
+        <div className="lane-header" onClick={this.activateLaneEdit}>
           <div className="lane-add-note">
             <button onClick={this.addNote}>+</button>
           </div>
-          <div className="lane-name">{lane.name}</div>
+          <Editable 
+            className="lane-name" 
+            editing={lane.editing}
+            value={lane.name}
+            onEdit={this.editName} />
+          <div className="lane-delete">
+            <button onClick={this.deleteLane}>x</button>
+          </div>
         </div>
         <AltContainer
           stores={[NoteStore]}
@@ -23,7 +31,10 @@ export default class Lane extends React.Component {
             notes: () => NoteStore.getNotesByIds(lane.notes)
           }}
         >
-          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+          <Notes
+            onValueClick={this.activateNoteEdit} 
+            onEdit={this.editNote} 
+            onDelete={this.deleteNote} />
         </AltContainer>
       </div>
     );
@@ -32,13 +43,19 @@ export default class Lane extends React.Component {
   editNote(id, task) {
     // don't modify if empty value
     if(!task.trim()) {
+      NoteActions.update({id, editing: false});
+      
       return;
     }
     
-    NoteActions.update({id, task});
+    NoteActions.update({id, task, editing: false});
   }
   
   addNote = (e) => {
+    // If note is added, avoid opening lane name edit by stopping
+    // event bubbling in this case.
+    e.stopPropagation();
+    
     const laneId = this.props.lane.id;
     const note = NoteActions.create({task: 'New task'});
     
@@ -55,6 +72,34 @@ export default class Lane extends React.Component {
     const laneId = this.props.lane.id;
     
     LaneActions.detachFromLane({laneId, noteId});
-    NoteActions.delete(id);
+    NoteActions.delete(noteId);
+  };
+  
+  editName = (name) => {
+    const laneId = this.props.lane.id;
+    
+    if (!name.trim()) {
+      LaneActions.update({id: laneId, editing: false});
+      
+      return;
+    }
+    
+    LaneActions.update({id: laneId, editing: false});
+  };
+  
+  deleteLane = () => {
+    const laneId = this.props.lane.id;
+    
+    LaneActions.delete(laneId);
+  };
+  
+  activateLaneEdit = () => {
+    const laneId = this.props.lane.id;
+
+    LaneActions.update({id: laneId, editing: true});
+  };
+  
+  activateNoteEdit(id) {
+    NoteActions.update({id, editing: true});
   }
 }
